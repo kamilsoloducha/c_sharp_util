823942c1457a3996f971707037b2f1b078c4087f
DS\KFG864 <kamil.soloducha@motorolasolutions.com>
2017-10-04 10:39:27 +0200

    add packageStore, ITask and TaskQueue

---
+++ Util/Util/PackageStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    public class PackageStore
+    {
+
+        private static Dictionary<int, object> _storeDictionary = new Dictionary<int, object>();
+
+        public static bool Put(int pKey, object pObject)
+        {
+            lock (_storeDictionary)
+            {
+                if (_storeDictionary.ContainsKey(pKey))
+                    return false;
+                _storeDictionary.Add(pKey, pObject);
+            }
+            return true;
+        }
+
+        public static object Get(int pKey)
+        {
+            object lResult;
+            lock (_storeDictionary)
+            {
+                if (!_storeDictionary.ContainsKey(pKey))
+                    return null;
+                lResult = _storeDictionary[pKey];
+                _storeDictionary.Remove(pKey);
+            }
+            return lResult;
+        }
+
+    }
+}
---
+++ Util/Util/Tasks/TaskQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Util.Tasks
+{
+    public class TaskQueue<T> where T : ITask
+    {
+
+        public LinkedList<T> Queue { get; private set; }
+
+        public Action<bool> OnCompleted { get; set; }
+
+        private volatile bool _isRunning;
+
+        public TaskQueue()
+        {
+            Queue = new LinkedList<T>();
+        }
+
+        public Task Execute()
+        {
+            _isRunning = true;
+            return Task.Run(() =>
+            {
+                while (Queue.Count >= 0)
+                {
+                    T task = Queue.First.Value;
+                    bool result = task.Execute();
+                    if (result && _isRunning)
+                    {
+                        Queue.RemoveFirst();
+                        continue;
+                    }
+                    Abort(false);
+                    return;
+                }
+                Abort(true);
+            });
+        }
+
+        public void Cancel()
+        {
+            _isRunning = true;
+        }
+
+        private void Abort(bool success)
+        {
+            if (OnCompleted != null)
+            {
+                OnCompleted(false);
+                
+            }
+            _isRunning = false;
+        }
+        
+    }
+}
