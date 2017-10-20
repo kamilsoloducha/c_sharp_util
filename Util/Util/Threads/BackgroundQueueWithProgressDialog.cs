05fec8d077eb34f65d2d46062c7157408347a7d8
DS\KFG864 <kamil.soloducha@motorolasolutions.com>
2017-10-20 15:47:24 +0200

    add backgroundQueueWithProgessDialog

---
+++ Util/Util/Threads/BackgroundQueueWithProgressDialog.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using Util.Threads;
+
+namespace Wordki.Helpers
+{
+    public class BackgroundQueueWithProgressDialog : BackgroundWorkQueue
+    {
+        public IProgressDialog Dialog { get; set; }
+
+        public BackgroundQueueWithProgressDialog() : base()
+        {
+        }
+
+        public override void Execute()
+        {
+            base.Execute();
+            Dialog.Show();
+        }
+
+        protected override void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            base.worker_DoWork(sender, e);
+            Application.Current.Dispatcher.Invoke(() => Dialog.Close());
+        }
+
+        public override void Abort()
+        {
+            base.Abort();
+            Dialog.Close();
+        }
+
+    }
+}
