68d96952e696721d3bff0b9de3b4cc6cce5e9870
vesper <kamilsoloducha@gmail.com>
2017-12-05 06:30:04 +0100

    Add collection extensitons

---
+++ Util/Util/Collections/CollectionExtensitons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Util.Collections
+{
+    public static class CollectionExtensitons
+    {
+
+        public static void AddRange<T>(this ICollection<T> destination, IEnumerable<T> source)
+        {
+            if(source == null)
+            {
+                return;
+            }
+            foreach(T item in source)
+            {
+                destination.Add(item);
+            }
+        }
+
+    }
+}
---
+++ Util/Util/DateTimeExtention.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Util
+{
+    public static class DateTimeExtention
+    {
+
+        public static DateTime GetDateTime(int year = 1990, int month = 9, int day = 24, int hour = 12, int min = 0, int sec = 0)
+        {
+            return new DateTime(year, month, day, hour, min, sec);
+        }
+
+    }
+}
